3d322432817454f6a85bf60a2067e0ef4487b385
Ana Caroline <155763806+ACPedrosa@users.noreply.github.com>
2025-06-25 03:12:32 +0000

    feat(hmac):  função para gerar  mensagem autenticada

--- HMAC/estacao.py
+++ HMAC/estacao.py
@@ -4,6 +4,24 @@
         - Aplicando HMAC
 """
 
-def autenticar_mensagem():
+# Dados da estação
 
-    return msg;
+dados = {
+    "temperatura": 25.3,
+    "umidade": 60.2,
+    "pressao": 1013.2,
+}
+
+def autenticar_mensagem(dados: dict, chave_secrete: bytes) -> bytes:
+    """
+    Gera uma mensagem autenticada com HMAC-SHA256.
+
+    Parâmetros:
+        dados (dict): dados da estação meteorológica.
+        chave (bytes): chave secreta usada para gerar o HMAC.
+
+    Retorno:
+        bytes: arquivo com os dados originais e o HMAC gerado.
+    """
+    mensagem = b'ana'
+    return mensagem;
