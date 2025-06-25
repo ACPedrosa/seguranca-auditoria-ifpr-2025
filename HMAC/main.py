7ea49ede9c28c22693472ff1de7957ff62b86d91
Ana Caroline <155763806+ACPedrosa@users.noreply.github.com>
2025-06-25 19:11:54 +0000

    docs(main): add ações do servidor

--- HMAC/main.py
+++ HMAC/main.py
@@ -1,19 +1,29 @@
 """
-    Arquivo Principal
+    -- Arquivo Principal --
 """
 from estacao import receber_dados_estacao, hash_senha, autenticar_mensagem, salvar_json
-#from servidor import
+from servidor import ler_json
 
+#---- Ações da estação ----
 senha = input("Digite sua senha: ")
 
-temperatura = 23.5
-umidade = 65
-pressao = 1012
+temperatura = 17.0
+umidade = 56
+pressao = 1000
 
 # Processamento
 dados = receber_dados_estacao(temperatura, umidade, pressao)
-chave = hash_senha(senha)
+salt, chave = hash_senha(senha)
 mensagem = autenticar_mensagem(dados, chave)
 
 # Salvando a mensagem
 salvar_json(mensagem)
+
+# ---- Ações do servidor ----
+
+caminho = "dados/dados_estacao.json"
+mensagem_server = ler_json(caminho)
+
+
+
+#validar o arquivo
